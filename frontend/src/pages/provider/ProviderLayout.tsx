202ca8e9b70f3777d331c98edf0e01deda077f33
Kurasao <jamerolenor@gmail.com>
2025-12-21 23:24:32 +0800

    feat: remove redundant px in providerlayout

--- frontend/src/pages/provider/ProviderLayout.tsx
+++ frontend/src/pages/provider/ProviderLayout.tsx
@@ -6,7 +6,7 @@ function ProviderLayout() {
     <>
       <Navbar />
 
-      <div className="pt-24 md:pt-30 pb-10 px-4 md:px-10 w-full min-h-screen bg-secondary">
+      <div className="pt-24 md:pt-30 pb-10 w-full min-h-screen bg-secondary">
         <Outlet />
       </div>
     </>
